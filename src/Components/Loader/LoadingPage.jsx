5fa556aec08653422785e6ce11184b4e5f3a9cc5
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2025-12-13 01:20:15 +0600

    UPDATE: stop scroll on loading

--- src/Components/Loader/LoadingPage.jsx
+++ src/Components/Loader/LoadingPage.jsx
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
+
 export default function LoadingPage() {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-white">
-      <div className="flex h-16 w-16 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-blue-400 text-4xl text-blue-400">
-        <div className="flex h-12 w-12 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-red-400 text-2xl text-red-400"></div>
+    <div className="fixed inset-0 flex items-center justify-center bg-white/80 backdrop-blur-sm z-50">
+      <div className="flex h-16 w-16 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-blue-400">
+        <div className="flex h-12 w-12 animate-spin items-center justify-center rounded-full border-4 border-transparent border-t-red-400"></div>
       </div>
     </div>
   );
